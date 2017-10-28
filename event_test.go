94fadbd3f0ba6344ee6821af4aea3abb5c81d672
Dominic Barnes <dominic@dbarnes.info>
2017-10-27 23:54:48 -0700

    feat(event): include original error for more context (#10)

--- event_test.go
+++ event_test.go
@@ -1,12 +1,15 @@
 package ecslogs
 
 import (
+	"fmt"
 	"io"
 	"syscall"
 	"testing"
 )
 
 func TestEvent(t *testing.T) {
+	unserializable := make(chan int) // a value that cannot be JSON-marshalled to ensure failure doesn't break everything
+
 	tests := []struct {
 		e Event
 		s string
@@ -17,11 +20,15 @@ func TestEvent(t *testing.T) {
 		},
 		{
 			e: Eprintf(WARN, "an error was raised (%s)", syscall.Errno(2)),
-			s: `{"level":"WARN","time":"0001-01-01T00:00:00Z","info":{"errors":[{"type":"syscall.Errno","error":"no such file or directory","errno":2}]},"data":{},"message":"an error was raised (no such file or directory)"}`,
+			s: `{"level":"WARN","time":"0001-01-01T00:00:00Z","info":{"errors":[{"type":"syscall.Errno","error":"no such file or directory","errno":2,"origError":2}]},"data":{},"message":"an error was raised (no such file or directory)"}`,
 		},
 		{
 			e: Eprint(ERROR, "an error was raised:", io.EOF),
-			s: `{"level":"ERROR","time":"0001-01-01T00:00:00Z","info":{"errors":[{"type":"*errors.errorString","error":"EOF"}]},"data":{},"message":"an error was raised: EOF"}`,
+			s: `{"level":"ERROR","time":"0001-01-01T00:00:00Z","info":{"errors":[{"type":"*errors.errorString","error":"EOF","origError":{}}]},"data":{},"message":"an error was raised: EOF"}`,
+		},
+		{
+			e: Eprint(ERROR, "an error was raised:", fakeError{"fail", unserializable}), // value that cannot be JSON-marshalled
+			s: fmt.Sprintf(`{"level":"ERROR","time":"0001-01-01T00:00:00Z","info":{},"data":{},"message":"an error was raised: {fail %v}"}`, unserializable),
 		},
 	}
 
@@ -30,4 +37,13 @@ func TestEvent(t *testing.T) {
 			t.Errorf("\n- expected: %s\n- found:    %s", test.s, s)
 		}
 	}
+}
+
+type fakeError struct {
+	msg   string
+	value interface{}
+}
+
+func (e *fakeError) Error() string {
+	return e.msg
 }
